821232af0470fa6e7c4df7a3d4cd988254287aa4
JoyNaraShin <joysihn93@gmail.com>
2020-09-23 17:59:42 +0900

    master branch

---
+++ src/views/MainPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="main list-container contents">
+      <h1 class="page-header">Today I Learned</h1>
+      <ul>
+        <PostListItem
+          v-for="post in postItems"
+          :key="post._id"
+          :item="post"
+        ></PostListItem>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { fetchPost } from '@/api/index';
+import PostListItem from '@/components/posts/PostListItem';
+export default {
+  components: { PostListItem },
+  data() {
+    return {
+      postItems: [],
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      //comiit
+      const { data } = await fetchPost();
+      this.postItems = data.posts;
+      console.log(data);
+    },
+  },
+};
+</script>
+
+<style></style>
